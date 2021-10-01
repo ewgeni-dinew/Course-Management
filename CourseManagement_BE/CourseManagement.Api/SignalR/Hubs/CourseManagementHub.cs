57d66b7c958172e2ce46a485da3ecdc36b04a2dc
Evgeni Dinev <ewgeni.dinew@gmail.com>
2021-10-01 18:29:54 +0300

    adding minor changes

--- CourseManagement_BE/CourseManagement.Api/SignalR/Hubs/CourseManagementHub.cs
+++ CourseManagement_BE/CourseManagement.Api/SignalR/Hubs/CourseManagementHub.cs
@@ -3,11 +3,18 @@
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.SignalR;
 
-    public class CourseManagementHub: Hub
+    public class CourseManagementHub : Hub
     {
         public async Task RemoveViewer(object data)
         {
+            // 'data' argument should contain both 'CourseId' and 'UserId'
             await Clients.All.SendAsync("removeViewer", data);
+        }
+
+        public async Task RemoveViewerOnLogout(object data)
+        {
+            // 'data' argument should contain only 'UserId'
+            await Clients.All.SendAsync("removeViewerOnLogout", data);
         }
     }
 }
